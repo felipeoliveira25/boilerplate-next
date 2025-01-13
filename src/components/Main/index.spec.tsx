137a7af570b001102e66c80c5174349f8c2a8f68
felipe.oliveira <felipe.oliveira@fcxlabs.com.br>
2025-01-13 11:34:13 -0300

    fix: :bug: Ajusta testes após erro no CI e ajusta jest.config para não cobrir stories

--- src/components/Main/index.spec.tsx
+++ src/components/Main/index.spec.tsx
@@ -4,7 +4,7 @@ import { Main } from '.'
 describe('<Main />', () => {
     it('should render the heading', () => {
         //renderiza o componente
-        const {container} = render(<Main/>)
+        const {container} = render(<Main title='Main para teste' description='teste main'/>)
 
         //busca o elemento e verifica a existência do mesmo
         expect(screen.getByRole('heading', { name: /main para teste/i })).toBeInTheDocument()
